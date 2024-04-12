459ac5da6215c0902a6b9dae8c28f1281303e144
simxnet <simxnet@envs.net>
2024-04-12 17:57:09 +0200

    fix(login-button): add button props

--- src/components/common/buttons/login-button.tsx
+++ src/components/common/buttons/login-button.tsx
@@ -1,16 +1,20 @@
-import { Button } from "@nextui-org/react";
+import { Button, type ButtonProps } from "@nextui-org/react";
 import Link from "next/link";
 import DiscordIcon from "../icons/discord";
 
-export default function LoginButton() {
+export default function LoginButton({
+	children = "Login",
+	...props
+}: ButtonProps) {
 	return (
 		<Button
+			{...props}
 			startContent={<DiscordIcon className="w-5 h-5" />}
 			as={Link}
 			href="/api/auth/login"
 			color="default"
 		>
-			Login
+			{children}
 		</Button>
 	);
 }
