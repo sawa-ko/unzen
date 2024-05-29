388e8bc618f1223d02f3b5ae6ec437548b8957e4
simxnet <simxnet@envs.net>
2024-05-29 19:31:48 +0200

    refactor: switch to zod

--- src/lib/dtos/submit-bot.ts
+++ src/lib/dtos/submit-bot.ts
@@ -1,43 +1,12 @@
-import { classValidatorResolver } from "@hookform/resolvers/class-validator";
-import { IsDefined, IsOptional, IsString, Length } from "class-validator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-export class SubmitBotDTO {
-	@IsString()
-	@Length(18, 19, {
-		message: "ID should be a valid Discord ID",
-	})
-	@IsDefined({
-		message: "ID is required",
-	})
-	public readonly id: string;
+export const submitBotSchema = z.object({
+	id: z.string().min(18).max(19),
+	shortDescription: z.string().min(25).max(100),
+	description: z.string().min(100).max(5_000),
+	prefix: z.string().max(10),
+	tags: z.string(),
+});
 
-	@IsString()
-	@Length(25, 100, {
-		message: "Short description length should be between 25 and 100",
-	})
-	@IsDefined({
-		message: "Short description is required",
-	})
-	public readonly shortDescription: string;
-
-	@IsString()
-	@Length(100, 5_000, {
-		message: "Description length should be between 100 and 5000",
-	})
-	@IsDefined({
-		message: "Description is required",
-	})
-	public readonly description: string;
-
-	@IsString()
-	@Length(0, 10, {
-		message: "Prefix length should be between 0 and 10",
-	})
-	@IsOptional()
-	public readonly prefix: string;
-
-	@IsString()
-	public readonly tags: string;
-}
-
-export const submitBotResolver = classValidatorResolver(SubmitBotDTO);
+export const submitBotResolver = zodResolver(submitBotSchema);
--- src/lib/dtos/webhook.ts
+++ src/lib/dtos/webhook.ts
@@ -1,35 +1,10 @@
 import { classValidatorResolver } from "@hookform/resolvers/class-validator";
-import {
-	ArrayMinSize,
-	IsArray,
-	IsNotEmpty,
-	IsOptional,
-	IsString,
-	IsUrl,
-} from "class-validator";
+import { z } from "zod";
 
-export class WebhookDTO {
-	@IsUrl({
-		host_blacklist: ["discord.com", "discordapp.com"],
-		protocols: ["https"],
-	})
-	@IsNotEmpty({
-		message: "Url is required",
-	})
-	url!: string;
+export const webhookSchema = z.object({
+	url: z.string().url(),
+	secret: z.string(),
+	events: z.array(z.string()).optional(),
+});
 
-	@IsString({
-		message: "Secret must be a valid string",
-	})
-	@IsNotEmpty({
-		message: "Secret is required",
-	})
-	secret!: string;
-
-	@IsOptional()
-	@IsArray()
-	@ArrayMinSize(1)
-	events!: string[];
-}
-
-export const webhookResolver = classValidatorResolver(WebhookDTO);
+export const webhookResolver = classValidatorResolver(webhookSchema);
