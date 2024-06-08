a2cf1b6ac5b6a26f59fb4afd9227571cabf03741
simxnet <simxnet@envs.net>
2024-06-08 19:32:20 +0200

    feat: custom toast style

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,15 @@ import { ApolloWrapper } from "./apollo-wrapper";
 export default function Providers({ children }: React.PropsWithChildren) {
 	return (
 		<ApolloWrapper>
-			<Toaster theme="dark" />
+			<Toaster
+				toastOptions={{
+					style: {
+						backgroundColor: "var(--colors-background-900)",
+						color: "var(--colors-background-100)",
+						borderColor: "var(--colors-background-700)",
+					},
+				}}
+			/>
 			{children}
 		</ApolloWrapper>
 	);
